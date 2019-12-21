5c60e737f45342b26f6180020115d44a4f9dbf9d
danman15726 <danman15726@gmail.com>
2019-12-20 22:51:50 -0600

    I added express and path as per requirements to server.js. I also added prelim code into the mix such as 2 app.get paths that i should probably move to htmlRoutes.js. app.listen is also here to listen to the port once the server is started.

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended:true}));
+app.use(express.json());
+
+
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+
+});
+
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+
+});
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+});
